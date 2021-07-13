00af3c2dc2ddcdefccabdf04398521e678540662
Daniel <duquedk1@gmail.com>
2021-07-13 16:03:33 -0500

    index.js completion

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ let filas=Number(prompt("Agrega el # de filas"))
 let colum=Number(prompt("Agrega el # de columnas"))
 
 console.log("filas "+filas+" Columnas "+colum)
+
+function matrixMaker(){
+  let ele=document.getElementById('matriz')
+  console.log(ele)
+  let tab="<table>"
+  for (let i=0;i<filas;i++) {
+    tab=tab+"<tr>"
+    for (var j=0;j<colum;j++) {
+      tab=tab+"<td></td>"
+    }
+    tab=tab+"</tr>"
+  }
+  tab=tab+"</table>"
+  ele.innerHTML= tab
+}
+
+matrixMaker()
